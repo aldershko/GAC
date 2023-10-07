9e904679b922373c8b68a492473ea3805c143026
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-10-07 15:52:04 +0530

    refactor: add services component layout

---
+++ gac_web/src/components/Services.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <>
+      <div className="container flex flex-col">
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-gacRed">Services</span>
+          <h1 className="text-gacBlue">The Services We Provide</h1>
+        </div>
+        <div className="flex flex-row">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="flex flex-row">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="flex flex-row justify-center items-center">
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
